7a554670e144a288baa956ddc2022b1c72ade34c
Alexander Storozhuk <60712071+Murzzik@users.noreply.github.com>
2022-07-14 22:32:35 +0300

    Lnked list practice Basic Operations on Linked List: Traversal, append, Insertion, Deletion, Searching, Updating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,126 @@
-import "./styles.css";
+/**
+ * Basic Operations on Linked List
+ * [x] Traversal: O(n) To traverse all the nodes one after another.
+ * [x] Append: To add a node at the end of list
+ * [ ] Insertion: To add a node at the given position.
+ * [ ] Deletion: To delete a node at the given position.
+ * [x] Searching: To search an element(s) by value.
+ * [x] Updating: To update a node at the given position.
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// [] -> [] -> [] -> [] (-> [])
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  getNext() {
+    return this.next;
+  }
+
+  setNext(next) {
+    this.next = next;
+  }
+
+  setValue(value) {
+    this.value = value;
+  }
+}
+
+class IndexOutOfBoundsError extends Error {
+  constructor(index) {
+    super(`Index out of bounds: ${index}`);
+  }
+}
+
+const head = new Node(1, new Node(2, new Node(3, new Node(4))));
+// console.log(list);
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  forEach(func) {
+    if (!this.head) return;
+
+    let current = this.head;
+
+    while (current) {
+      func(current.getValue());
+      current = current.getNext();
+    }
+  }
+
+  // [] -> [] -> ['hello'] -> []
+  update(index, value) {
+    if (index < 0) throw new IndexOutOfBoundsError(index);
+    if (!this.head) throw new IndexOutOfBoundsError(index);
+
+    let current = this.head;
+    let i = 0;
+
+    while (current) {
+      if (i === index) {
+        current.setValue(value);
+        return;
+      }
+      current = current.getNext();
+      i++;
+    }
+
+    if (i < index) throw new IndexOutOfBoundsError(index);
+  }
+
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.getNext()) {
+      current = current.getNext();
+    }
+
+    current.setNext(new Node(value));
+  }
+
+  find(predicate) {
+    if (!this.head) return false;
+
+    let current = this.head;
+
+    while (current) {
+      if (predicate(current.getValue())) {
+        return current;
+      }
+
+      current = current.getNext();
+    }
+
+    return false;
+  }
+}
+
+const list = new LinkedList(head);
+// console.log(list);
+// list.forEach(v => console.log(v.toString()));
+
+const empty = new LinkedList();
+empty.append("lol");
+// empty.forEach(console.info);
+empty.append("kek");
+// empty.forEach(console.info);
+empty.append("hello");
+empty.append("Alex");
+empty.append("!");
+
+// console.log(empty.find(v => v === 'hello'));
